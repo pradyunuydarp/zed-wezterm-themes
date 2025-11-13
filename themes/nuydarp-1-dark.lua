ee767ecd0c480840a15193ecc86bf2a932eaa56f
pradyunuydarp <pradyundevarakonda24@gmail.com>
2025-11-13 15:29:11 +0530

    New Themes

---
+++ themes/nuydarp-1-dark.lua
@@ -0,0 +1,29 @@
+return {
+  background = '#000000',
+  foreground = '#EAEAEA',
+  cursor_bg = '#CFB53B',
+  cursor_fg = '#000000',
+  cursor_border = '#CFB53B',
+  selection_bg = '#CFB53B',
+  selection_fg = '#000000',
+  ansi = {
+    '#000000', -- black
+    '#C75450', -- red
+    '#8F9779', -- green
+    '#CFB53B', -- yellow
+    '#5F9EA0', -- blue
+    '#C8A2C8', -- magenta
+    '#87CEEB', -- cyan
+    '#EAEAEA', -- white
+  },
+  brights = {
+    '#555555', -- bright black
+    '#D96E6A', -- bright red
+    '#A8B094', -- bright green
+    '#E0CA60', -- bright yellow
+    '#7AC5C8', -- bright blue
+    '#DABADD', -- bright magenta
+    '#A0D8F0', -- bright cyan
+    '#FFFFFF', -- bright white
+  },
+}
